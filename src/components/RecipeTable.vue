ef3911efb936a746bc15fd50fe52892cf41ec256
Jess <sepshuncontact@gmail.com>
2021-06-17 03:53:20 -0400

    feat(RecipeTable): sticky header
    Header will stick when scrolling down the page

--- src/components/RecipeTable.vue
+++ src/components/RecipeTable.vue
@@ -1,58 +1,60 @@
 <template>
-  <div class="recipe-table">
+  <div class="recipe-table" :class="{'-lockheader': lockHeader}">
     <!-- TABLE HEADER -->
     <div class="header">
-      <div class="filter">
-        <input class="input" v-model="currentFilter" placeholder="Filter Recipes">
-        <div class="count">
-          {{ sortedData.length }} result<template v-if="sortedData.length !== 1">s</template>
-        </div>
-      </div>
-
-      <div class="columns">
-        <div>Item Name</div>
-        <div>Ingredients</div>
-
-        <!-- HUNGER HEADER -->
-        <div
-          class="-center -clickable"
-          :class="{
-            '-active': currentSort === 'hunger',
-            '-asc': currentSortDir === 'asc'
-          }"
-          @click="sort('hunger')"
-        >
-          <img src="@/assets/items/HungerMeter.png">
-          Hunger
-          <i class="material-icons sort-icon">arrow_drop_down</i>
+      <div class="container">
+        <div class="filter">
+          <input class="input" v-model="currentFilter" placeholder="Filter Recipes">
+          <div class="count">
+            {{ sortedData.length }} result<template v-if="sortedData.length !== 1">s</template>
+          </div>
         </div>
 
-        <!-- HEALTH HEADER -->
-        <div
-          class="-center -clickable"
-          :class="{
-            '-active': currentSort === 'health',
-            '-asc': currentSortDir === 'asc'
-          }"
-          @click="sort('health')"
-        >
-          <img src="@/assets/items/HealthMeter.png">
-          Health
-          <i class="material-icons sort-icon">arrow_drop_down</i>
-        </div>
+        <div class="columns">
+          <div>Item Name</div>
+          <div>Ingredients</div>
 
-        <!-- SANITY HEADER -->
-        <div
-          class="-center -clickable"
-          :class="{
-            '-active': currentSort === 'sanity',
-            '-asc': currentSortDir === 'asc'
-          }"
-          @click="sort('sanity')"
-        >
-          <img src="@/assets/items/SanityMeter.png">
-          Sanity
-          <i class="material-icons sort-icon">arrow_drop_down</i>
+          <!-- HUNGER HEADER -->
+          <div
+            class="-center -clickable"
+            :class="{
+              '-active': currentSort === 'hunger',
+              '-asc': currentSortDir === 'asc'
+            }"
+            @click="sort('hunger')"
+          >
+            <img src="@/assets/items/HungerMeter.png">
+            Hunger
+            <i class="material-icons sort-icon">arrow_drop_down</i>
+          </div>
+
+          <!-- HEALTH HEADER -->
+          <div
+            class="-center -clickable"
+            :class="{
+              '-active': currentSort === 'health',
+              '-asc': currentSortDir === 'asc'
+            }"
+            @click="sort('health')"
+          >
+            <img src="@/assets/items/HealthMeter.png">
+            Health
+            <i class="material-icons sort-icon">arrow_drop_down</i>
+          </div>
+
+          <!-- SANITY HEADER -->
+          <div
+            class="-center -clickable"
+            :class="{
+              '-active': currentSort === 'sanity',
+              '-asc': currentSortDir === 'asc'
+            }"
+            @click="sort('sanity')"
+          >
+            <img src="@/assets/items/SanityMeter.png">
+            Sanity
+            <i class="material-icons sort-icon">arrow_drop_down</i>
+          </div>
         </div>
       </div>
     </div>
@@ -91,18 +93,29 @@ export default defineComponent({
   },
   data () {
     return {
+      lockHeader: false,
+
       currentFilter: '' as string,
       currentSort: 'hunger' as SortField,
       currentSortDir: 'desc' as SortDirection
     }
   },
   methods: {
+    // Change Sorting Field and Direction
     sort (s: SortField) {
       if (s === this.currentSort) this.currentSortDir = this.currentSortDir === 'asc' ? 'desc' : 'asc'
       this.currentSort = s
+    },
+
+    // Calculate LockHeader when Scrolling
+    scrollLockHeader (event: Event) {
+      const scrollPos = (event.target as HTMLDivElement)?.scrollTop
+      if (scrollPos >= this.$el.offsetTop) this.lockHeader = true
+      else this.lockHeader = false
     }
   },
   computed: {
+    // Sort and Filter Recipe Data
     sortedData (): IRecipe[] {
       const res = this.data
       return res.sort((a, b) => {
@@ -119,6 +132,11 @@ export default defineComponent({
         })
       })
     }
+  },
+  created () {
+    // Register Scroll Event Listener
+    const appEl = document.getElementById('app')
+    if (appEl) appEl.addEventListener('scroll', e => window.requestAnimationFrame(() => this.scrollLockHeader(e)))
   }
 })
 </script>
@@ -133,68 +151,83 @@ export default defineComponent({
   display: grid;
   grid-template-rows: 104px auto;
 
+  &.-lockheader {
+    grid-template-rows: auto;
+
+    > .header > .container {
+      position: absolute;
+      top: 0;
+      width: 1000px;
+
+      box-shadow: 0 8px 16px rgba(0,0,0,0.2);
+    }
+  }
   // HEADER
   > .header {
-    background: hsl(220,10%,20%);
-    box-shadow: 0 8px 16px rgba(0,0,0,0.2);
+    min-height: 104px;
 
-    > .filter {
-      display: grid;
-      grid-template-columns: 1fr auto;
+    > .container {
+    background: hsl(220,10%,18%);
+    transition: 0.15s ease;
 
-      > .input {
-        background: transparent;
-        padding: 16px;
-        border: none;
-        width: 100%;
-        box-sizing: border-box;
+      > .filter {
+        display: grid;
+        grid-template-columns: 1fr auto;
 
-        &:focus {
-          outline: none;
-          background: hsla(220, 10%, 0%, 0.1);
+        > .input {
+          background: transparent;
+          padding: 16px;
+          border: none;
+          width: 100%;
+          box-sizing: border-box;
+
+          &:focus {
+            outline: none;
+            background: hsla(220, 10%, 0%, 0.1);
+          }
+        }
+
+        > .count {
+          padding: 16px;
+          justify-self: center;
+          opacity: 0.5;
         }
       }
 
-      > .count {
-        padding: 16px;
-        justify-self: center;
-        opacity: 0.5;
-      }
-    }
+      > .columns {
+        display: grid;
+        grid-template-columns: 280px 288px 1fr 1fr 1fr;
 
-    > .columns {
-      display: grid;
-      grid-template-columns: 280px 288px 1fr 1fr 1fr;
+        > div {
+          position: relative;
+          padding: 16px;
+          align-self: center;
 
-      > div {
-        position: relative;
-        padding: 16px;
-        align-self: center;
+          font-weight: 500;
 
-        font-weight: 500;
+          > img { vertical-align: bottom; }
 
-        > img { vertical-align: bottom; }
+          &.-center {
+            padding: 16px 0;
+            text-align: center;
+          }
+          &.-clickable {
+            cursor: pointer;
+            transition: 0.15s ease;
+            &:hover { background: hsla(220,10%,50%,0.2); }
+          }
 
-        &.-center {
-          padding: 16px 0;
-          text-align: center;
+          > .sort-icon {
+            position: absolute;
+            bottom: -4px;
+            left: 50%;
+            transform: translate(-50%, 0%);
+            opacity: 0;
+            transition: 0.15s ease;
+          }
+          &.-active > .sort-icon { opacity: 1; }
+          &.-asc > .sort-icon { transform: translate(-50%, 0%) rotate(180deg); }
         }
-        &.-clickable {
-          cursor: pointer;
-          transition: 0.15s ease;
-          &:hover { background: hsla(220,10%,50%,0.2); }
-        }
-
-        > .sort-icon {
-          position: absolute;
-          bottom: -4px;
-          left: 50%;
-          transform: translate(-50%, 0%);
-          opacity: 0;
-          transition: 0.15s ease;
-        }
-        &.-active > .sort-icon { opacity: 1; }
-        &.-asc > .sort-icon { transform: translate(-50%, 0%) rotate(180deg); }
       }
     }
   }
